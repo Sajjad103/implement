134654e6886666ec69f4f254d6cae5b8fd0445f0
Sajjad103 <sajjadali.hyd@gmail.com>
2022-02-12 10:23:28 +0500

    added DrawerActivity commit

---
+++ app/src/main/java/com/example/android/modern/androidcomlpetearchitecture/allexamples/DrawerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.modern.androidcomlpetearchitecture.allexamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.modern.androidcomlpetearchitecture.R
+
+class DrawerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawer)
+    }
+}
